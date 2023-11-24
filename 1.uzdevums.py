4fa076f67815717d8a8a769874fedeeed0332f32
TomsSkabarnieks <toms.skabarnieks@grobinasskola.lv>
2023-11-24 09:45:55 +0200

    DECEMBRIS

---
+++ 1.uzdevums.py
@@ -0,0 +1,2 @@
+for skaitit in range(1, 50):
+    print(skaitit)
---
+++ 3.uzdevums.py
@@ -0,0 +1,6 @@
+skaitlis =(21)
+
+if skaitlis % 3 == 0 and skaitlis % 7 == 0:
+    print(f"{skaitlis} dalās ar 3 un 7")
+else:
+    print(f"{skaitlis} nedalās nearvienu")
